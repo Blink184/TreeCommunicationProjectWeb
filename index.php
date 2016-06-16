ea5bbb7fcee41238160370b22838c07c39f235d3
ahmmmoud <ahmmmoud@gmail.com>
2016-06-16 23:42:16 +0300

    adding files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmad hammoud
+ * Date: 6/16/2016
+ * Time: 10:26 PM
+ */
+?>
+<html>
+    <head>
+        <link href="css/global.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <ul id="ulMain">
+            <li>
+<!--                --><?php //include '_menu.html';?>
+                <div>a</div>
+            </li>
+            <li>
+
+<!--                --><?php //include '_header.html';?>
+
+
+            </li>
+        </ul>
+    </body>
+</html>
