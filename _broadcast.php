d89aea15768a425fa6598b9d0ba8f8568212fdf6
Aynur <aynur182@gmail.com>
2016-06-18 02:03:04 +0300

    _broadcastmsg half done

---
+++ _broadcast.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kirby
+ * Date: 6/16/2016
+ * Time: 11:27 PM
+ */
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="css/_broadcastMsg.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div id="motherDiv">
+    <div class="brdcstMsg" id="top">
+        <img src="" id="img" width="60px" height="60px"/>
+        <span id="empName">
+            Jack Black
+        </span>
+        <span id="timeReceived">
+            5 minutes ago
+        </span>
+        <div id="empRole">
+            Administrator
+        </div>
+    </div>
+    <div class="brdcstMsg" id="msg">
+        <div class="brdcstMsg" id="title">
+            This is a Title
+        </div>
+        <div class="brdcstMsg" id="content">
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+        </div>
+    </div>
+</div>
+</body>
+</html>
