a5e49e98a41e2caf63f6e3158147ace7b6567018
Aynur <aynur182@gmail.com>
2016-06-19 16:11:50 +0300

    broadcast page done

--- _broadcast.php
+++ _broadcast.php
@@ -1,42 +1,33 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Kirby
- * Date: 6/16/2016
- * Time: 11:27 PM
- */
-
-?>
-
-
 <!DOCTYPE html>
+<!--/**-->
+<!--* Created by PhpStorm.-->
+<!--* User: Kirby-->
+<!--* Date: 6/16/2016-->
+<!--* Time: 11:27 PM-->
+<!--*/-->
 <html lang="en">
 <head>
-    <link href="css/_broadcastMsg.css" rel="stylesheet" type="text/css"/>
+    <link href="css/_broadcast.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-<div id="motherDiv">
-    <div class="brdcstMsg" id="top">
-        <img src="" id="img" width="60px" height="60px"/>
-        <span id="empName">
-            Jack Black
+<div id="divBroadcasts">
+    <div id="divTop">
+        <span class="spanLeft">
+            <ul id="ulMidList">
+                <li>
+                    Received
+                </li>
+                <li>
+                    Sent
+                </li>
+            </ul>
         </span>
-        <span id="timeReceived">
-            5 minutes ago
-        </span>
-        <div id="empRole">
-            Administrator
-        </div>
     </div>
-    <div class="brdcstMsg" id="msg">
-        <div class="brdcstMsg" id="title">
-            This is a Title
-        </div>
-        <div class="brdcstMsg" id="content">
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        </div>
+    <div id="divMid">
+
     </div>
+<div id="contentDiv">
+    <?php include '_broadcastMsg.html';?>
 </div>
 </body>
 </html>
