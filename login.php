f83ec4d68f722eb985c040d2c40d64fe0d3e8c54
Aynur <aynur182@gmail.com>
2016-07-01 04:27:46 +0300

    Login page done.

--- login.php
+++ login.php
@@ -13,30 +13,45 @@
         <div class="middle">
             <div class="inner">
                 <div id="header">
-                    <h1>Tree Communiction</h1>
+                    <img id="treeLeftImg" src="resources/images/tree_left.png"/>
+                    <img id="treeRightImg" src="resources/images/tree_right.png"/>
+                    <div id="middleHeader">
+                        <img id="treeMidImg" src="resources/images/global/tree_logo.svg"/>
+                        <h3>Tree Communication</h3>
+                    </div>
                 </div>
                 <div id="body">
                     <ul>
                         <li>
                             <ul>
-                                <li><label for="username">Username</label></li>
+                                <li><img id="usernameImg" src="resources/images/user.png"/> </li>
                                 <li>
-                                    <input id="userName" type="text"/>
+                                    <input id="username" placeholder="Username" type="text"/>
                                 </li>
                             </ul>
                         </li>
                         <li>
                             <ul>
-                                <li><label for="password">Password</label></li>
+                                <li><img id="lockImg" src="resources/images/lock.png"/> </li>
                                 <li>
-                                    <input id="passWord" type="password"/>
+                                    <input id="password" placeholder="Password" type="password"/>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <ul>
+                                <li>
+                                    <input id="keepSignedInCB" type="checkbox"/>Keep me signed in
                                 </li>
                             </ul>
                         </li>
                     </ul>
                 </div>
                 <div id="footer">
-                    <button href="employees.php">Login</button>
+                    <button onclick="goToEmployeesPage();">Sign in</button>
+                    <br>
+                    <br>
+                    <a href="" for="forgotPassword">Forgot Password?</a>
                 </div>
             </div>
         </div>
