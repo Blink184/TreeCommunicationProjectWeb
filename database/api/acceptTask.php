0bab9217de9a867cd776067595afa8a90a207ec4
ahmmmoud <ahmmmoud@gmail.com>
2016-07-16 03:26:16 +0300

    adding acceptTask and finishTask api

---
+++ database/api/acceptTask.php
@@ -0,0 +1,16 @@
+<?php
+
+include '../task.php';
+
+if(empty($_REQUEST['taskid'])
+    || empty($_REQUEST['date']))
+{
+    echo 'missing parameters';
+    return;
+}
+
+$taskid = $_REQUEST['taskid'];
+$date = $_REQUEST['date'];
+
+echo acceptTask($taskid, $date);
+?>
