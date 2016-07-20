eb86689994b9915052f32ca32921af60ef72419a
ahmmmoud <ahmmmoud@gmail.com>
2016-07-20 23:16:35 +0300

    making messages page dynamic (not finished yet)

---
+++ database/api/getContacts.php
@@ -0,0 +1,14 @@
+<?php
+include '../message.php';
+
+if(empty($_REQUEST['userroleid']))
+{
+    echo encode(false, 'missing parameters');
+    return;
+}
+
+$userroleid = $_REQUEST['userroleid'];
+
+
+echo getLastMessages($userroleid);
+?>
