578fc38b1c6e46ba4858e1b6f937ce348b53cbe2
Aynur <aynur182@gmail.com>
2016-08-05 14:35:08 +0300

    changing api not done yet... not tested either

--- database/user.php
+++ database/user.php
@@ -3,71 +3,140 @@
 require_once 'connection.php';
 
 function insertUser($firstname, $lastname, $username, $password, $phone, $address, $email, $isAdmin, $lastActiveDate, $isLoggedIn, $isBanned, $isDeleted){
-    if(!usernameExists($username)){
-        $q = "INSERT INTO user (FirstName, LastName, Username, Password, Phone, Address, Email, IsAdmin, LastActiveDate, IsLoggedIn, IsBanned, IsDeleted) VALUES ('$firstname', '$lastname', '$username', '$password', '$phone', '$address', '$email', $isAdmin, '$lastActiveDate', $isLoggedIn, $isBanned, $isDeleted)";
-        return encode(execute($q), '');
+    $conn = connect();
+    $true = 1;
+    if(!usernameExists($username)) {
+        if ($stmt = $conn->prepare("INSERT INTO user (FirstName, LastName, Username, Password, Phone, Address, Email, IsAdmin, LastActiveDate, IsLoggedIn, IsBanned, IsDeleted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+            $stmt->bind_param("sssssssisiii", $firstname, $lastname, $username, $password, $phone, $address, $email, $isAdmin, $lastActiveDate, $isLoggedIn, $isBanned, $isDeleted);
+            return encode($stmt->execute(), '');
+        } else {
+            return encode(false, var_dump($conn->error));
+        }
     }else{
         return encode(false, 'username exists');
     }
 }
 
 function validateUser($username, $password) {
-    $q = "select u.*, ur.UserRoleId, r.IsMaster from user u, userrole ur, role r where ur.UserId = u.UserId and ur.RoleId = r.RoleId and u.Username ='$username' and BINARY u.Password = '$password'";
-    $res = execute($q);
-    if(any($res)){
-        return encode(true, firstRow($res));
+    $conn = connect();
+    $q = "select u.*, ur.UserRoleId, r.IsMaster from user u, userrole ur, role r where ur.UserId = u.UserId and ur.RoleId = r.RoleId and u.Username = ? and BINARY u.Password = ?";
+    if ($stmt = $conn->prepare($q)) {
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        if ($res->num_rows > 0) {
+            return encode(true, $res);
+        } else {
+            return encode(false, 'invalid username/password');
+        }
+    } else {
+        return encode(false, var_dump($conn->error));
     }
-    return encode(false, 'invalid username/password');
 }
 
 function getUsers(){
-    $q = "select * from user where IsDeleted = 0 and IsAdmin = 0 ORDER BY FirstName, LastName";
-    $tmp = execute($q);
-    $users = array();
-    while ($row = $tmp->fetch_assoc()) {
-        array_push($users, $row);
+    $conn = connect();
+    if ($stmt = $conn->prepare("select * from user where IsDeleted = 0 and IsAdmin = 0 ORDER BY FirstName, LastName")) {
+        $stmt->execute();
+        $rows = $stmt->get_result();
+        $users = array();
+        while ($row = $rows->fetch_assoc()) {
+            array_push($users, $row);
+        }
+        return encode(true, json_encode($users));
+    } else {
+        return encode(false, var_dump($conn->error));
     }
-    $users = json_encode($users);
-    return encode(true, $users);
 }
 
 function getUnassignedUsers(){
-    $q = "select * from user where IsDeleted = 0 and IsAdmin = 0 and UserId not in (select ur.UserId from userrole ur where ur.IsDeleted = 0) ORDER BY FirstName, LastName";
-    $tmp = execute($q);
-    $users = array();
-    while ($row = $tmp->fetch_assoc()) {
-        array_push($users, $row);
+    $conn = connect();
+    if ($stmt = $conn->prepare("select * from user where IsDeleted = 0 and IsAdmin = 0 and UserId not in (select ur.UserId from userrole ur where ur.IsDeleted = 0) ORDER BY FirstName, LastName")) {
+        $stmt->execute();
+        $rows = $stmt->get_result();
+        $users = array();
+        while ($row = $rows->fetch_assoc()) {
+            array_push($users, $row);
+        }
+        return encode(true, json_encode($users));
+    } else {
+        return encode(false, var_dump($conn->error));
     }
-    $users = json_encode($users);
-    return encode(true, $users);
 }
 
 function getUser($id){
-    $q = "select * from user where UserId = $id";
-    return encode(true, firstRow(execute($q)));
+    $conn = connect();
+    if ($stmt = $conn->prepare("select * from user where UserId = ?")) {
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        return encode(true, firstRow($stmt->get_result()));
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
 }
+
 function getUserByUsername($username){
-    $q = "select * from user where Username = '$username' and IsDeleted = 0";
-    return execute($q);
+    $conn = connect();
+    $false = 0;
+    if ($stmt = $conn->prepare("select * from user where Username = ? and IsDeleted = ?")) {
+        $stmt->bind_param("i", $username, $false);
+        $stmt->execute();
+        return encode(true, firstRow($stmt->get_result()));
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
 }
+
 function usernameExists($username){
     return any(getUserByUsername($username));
 }
 
 function updateUser($userId, $firstname, $lastname, $phone, $address, $username, $password){
-    $q = "update user set FirstName = '$firstname', LastName = '$lastname', Phone = '$phone', Password = '$password', Username = '$username', Address = '$address' where UserId = $userId";
-    return execute($q);
+    $conn = connect();
+    if ($stmt = $conn->prepare("update user set FirstName = ?, LastName = ?, Phone = ?, Password = ?, Username = ?, Address = ? where UserId = ?")) {
+        $stmt->bind_param("ssssssi", $firstname, $lastname, $phone, $password, $username, $address, $userId);
+        return encode($stmt->execute(), '');
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
 }
+
+//checkUsernameAvailability(1, 'ah');
 function checkUsernameAvailability($userId, $username){
-    $q = "select * from user where UserId <> $userId and Username = '$username'";
-    return !any(execute($q));
+    $conn = connect();
+    if ($stmt = $conn->prepare("select * from user where UserId <> ? and Username = ?")) {
+        $stmt->bind_param("is", $userId, $username);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        if ($res->num_rows == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
+//    $q = "select * from user where UserId <> $userId and Username = '$username'";
+//    return !any(execute($q));
 }
+
 function updateUserImage($userId, $image){
-    $q = "update user set Image = '$image' where UserId = $userId";
-    return execute($q);
+    $conn = connect();
+    if ($stmt = $conn->prepare("update user set Image = ? where UserId = ?")) {
+        $stmt->bind_param("si", $image, $userId);
+        return encode($stmt->execute(), '');
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
 }
+
 function deleteUser($userId){
-    $q = "update user set IsDeleted = 1 where UserId = $userId";
-    return encode(execute($q), '');
+    $conn = connect();
+    if ($stmt = $conn->prepare("update user set IsDeleted = 1 where UserId = ?")) {
+        $stmt->bind_param("i", $userId);
+        return encode($stmt->execute(), '');
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
 }
 
--- database/userRole.php
+++ database/userRole.php
@@ -4,15 +4,18 @@ require_once 'connection.php';
 require_once 'userRoleHierarchy.php';
 
 function insertUserRole($userId, $roleId, $parentId, $title, $isDeleted){
+    $conn = connect();
     if(!userRoleExists($userId, $roleId)){
-        $q = "INSERT INTO userrole (UserId, RoleId, Title, IsDeleted) VALUES ($userId, $roleId, '$title', $isDeleted)";
-        if(execute($q)){
-            $userRole = getLastUserRole()->fetch_assoc();
-            //return encode(true, '');
-            return insertUserRoleHierarchy($parentId, $userRole['UserRoleId']);
-
-        }else{
-            return encode(false, 'ERR_CODE_0002');
+        if ($stmt = $conn->prepare("INSERT INTO userrole (UserId, RoleId, Title, IsDeleted) VALUES (?, ?, ?, ?)")) {
+            $stmt->bind_param("iisi", $userId, $roleId, $title, $isDeleted);
+            if ($stmt->execute()) {
+                $userRole = getLastUserRole()->fetch_assoc();
+                return insertUserRoleHierarchy($parentId, $userRole['UserRoleId']);
+            } else {
+                return encode(false, "ERR_CODE_002");
+            }
+        } else {
+            return encode(false, var_dump($conn->error));
         }
     }else{
         return encode(false, 'User already has this role');
@@ -20,9 +23,14 @@ function insertUserRole($userId, $roleId, $parentId, $title, $isDeleted){
 }
 
 function updateUserRole($userRoleId, $userId, $roleId, $title){
+    $conn = connect();
     if(userRoleExistsWithId($userRoleId, $userId, $roleId) || !userRoleExists($userId, $roleId)){
-        $q = "update userrole set UserId = $userId, RoleId = $roleId, Title = '$title' where UserRoleId = $userRoleId";
-        return encode(execute($q), '');
+        if ($stmt = $conn->prepare("update userrole set UserId = ?, RoleId = ?, Title = ? where UserRoleId = ?")) {
+            $stmt->bind_param("iisi", $userId, $roleId, $title, $userRoleId);
+            return encode($stmt->execute(), '');
+        } else {
+            return encode(false, var_dump($conn->error));
+        }
     }else{
         return encode(false, 'User already has this role');
     }
@@ -30,21 +38,59 @@ function updateUserRole($userRoleId, $userId, $roleId, $title){
 
 
 function getLastUserRole(){
-    $q = "select * from UserRole where IsDeleted = 0 order by UserRoleId desc limit 1";
-    return execute($q);
+    $conn = connect();
+    if ($stmt = $conn->prepare("select * from UserRole where IsDeleted = 0 order by UserRoleId desc limit 1")) {
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return encode(true, ($res->fetch_assoc()));
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
+//    $q = "select * from UserRole where IsDeleted = 0 order by UserRoleId desc limit 1";
+//    return execute($q);
 }
 
 function getUserRoleWithId($userRoleId, $userId, $roleId){
-    $q = "select * from UserRole where UserId = $userId and RoleId = $roleId and UserRoleId = $userRoleId and IsDeleted = 0";
-    return execute($q);
+    $conn = connect();
+    if ($stmt = $conn->prepare("select * from UserRole where UserId = ? and RoleId = ? and UserRoleId = ? and IsDeleted = 0")) {
+        $stmt->bind_param("iii", $userId, $roleId, $userRoleId);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return encode(true, ($res->fetch_assoc()));
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
+//    $q = "select * from UserRole where UserId = $userId and RoleId = $roleId and UserRoleId = $userRoleId and IsDeleted = 0";
+//    return execute($q);
 }
 function getUserRole($userId, $roleId){
-    $q = "select * from UserRole where UserId = $userId and RoleId = $roleId and IsDeleted = 0";
-    return execute($q);
+    $conn = connect();
+    if ($stmt = $conn->prepare("select * from UserRole where UserId = ? and RoleId = ? and IsDeleted = 0")) {
+        $stmt->bind_param("ii", $userId, $roleId);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return encode(true, ($res->fetch_assoc()));
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
+//    $q = "select * from UserRole where UserId = $userId and RoleId = $roleId and IsDeleted = 0";
+//    return execute($q);
 }
+
+//getUserRoleById(1);
 function getUserRoleById($userRoleId){
-    $q = "select * from userrole ur, user u, role r where u.UserId = ur.UserId and r.RoleId = ur.RoleId and UserRoleId = $userRoleId";
-    return encode(true, firstRow(execute($q)));
+    $conn = connect();
+    if ($stmt = $conn->prepare("select * from userrole ur, user u, role r where u.UserId = ur.UserId and r.RoleId = ur.RoleId and UserRoleId = ?")) {
+        $stmt->bind_param("i", $userRoleId);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return encode(true, ($res->fetch_assoc()));
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
+//    $q = "select * from userrole ur, user u, role r where u.UserId = ur.UserId and r.RoleId = ur.RoleId and UserRoleId = $userRoleId";
+//    print_r(firstRow(execute($q)));
+//    return encode(true, firstRow(execute($q)));
 }
 
 function userRoleExists($userId, $roleId){
@@ -56,51 +102,45 @@ function userRoleExistsWithId($userRoleId, $userId, $roleId){
 
 
 function deleteUserRole($userRoleId){
-    $q = "UPDATE userrole set IsDeleted = 1 where UserRoleId = $userRoleId";
-    return encode(execute($q), '');
+    $conn = connect();
+    if ($stmt = $conn->prepare("UPDATE userrole set IsDeleted = 1 where UserRoleId = ?")) {
+        $stmt->bind_param("i", $userRoleId);
+        return encode($stmt->execute(), '');
+    } else {
+        return encode(false, var_dump($conn->error));
+    }
+//    $q = "UPDATE userrole set IsDeleted = 1 where UserRoleId = $userRoleId";
+//    return encode(execute($q), '');
 }
 
 function getUserRoles(){
     $q = "select ur.UserRoleId, u.FirstName, u.LastName, r.Description as Role from userrole ur, user u, Role r where ur.IsDeleted = 0 and ur.RoleId = r.RoleId and u.IsDeleted = 0 and u.UserId = ur.UserId ORDER BY FirstName, LastName";
-
-    $res = array();
-    $rows = execute($q);
-    while ($row = $rows->fetch_assoc()) {
-        array_push($res, $row);
+    $conn = connect();
+    if ($stmt = $conn->prepare($q)) {
+        $stmt->execute();
+        $res = array();
+        $rows = $stmt->get_result();
+        $stmt->close();
+        while ($row = $rows->fetch_assoc()) {
+            array_push($res, $row);
+        }
+        return encode(true, json_encode($res));
+    } else {
+        return encode(false, var_dump($conn->error));
     }
-
-    return encode(true, $res);
+    //return encode(true, $res);
 }
-
 
 function getUserRoleTree($id){
-    $sql="SELECT * FROM Role r, UserRole ur LEFT JOIN User u ON u.UserId = ur.UserId WHERE ur.RoleId = r.RoleId and ur.UserRoleId = $id";
+    $q="SELECT * FROM Role r, UserRole ur LEFT JOIN User u ON u.UserId = ur.UserId WHERE ur.RoleId = r.RoleId and ur.UserRoleId = ?";
+    $conn = connect();
+    if ($stmt = $conn->prepare($q)) {
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        $row = $res->fetch_assoc();
+        $stmt->close();
 
-    $row = execute($sql)->fetch_assoc();
-    $o['UserRoleId'] = $row['UserRoleId'];
-    $o['FirstName'] = $row['FirstName'];
-    $o['LastName'] = $row['LastName'];
-    $o['Name'] = $row['FirstName'] . ' ' . $row['LastName'];
-    $o['Username'] = $row['Username'];
-    $o['Address'] = $row['Address'];
-    $o['Phone'] = $row['Phone'];
-    $o['Role'] = $row['Description'];
-    $o['Image'] = $row['Image'];
-    $o['LastActiveDate'] = $row['LastActiveDate'];
-    $o['Title'] = $row['Title'];
-    $o['UserId'] = $row['UserId'];
-    $o['IsAssigned'] = !empty($row['UserId']);
-    $o['RoleId'] = $row['RoleId'];
-    $o['Children'] = array();
-    $o['Children'] = getUserRoleChildren($o['Children'], $o['UserRoleId']);
-    return encode(true, $o);
-}
-
-function getUserRoleChildren($children, $parentId) {
-    $children = array();
-    $sql="SELECT *, (select count(*) from UserRoleHierarchy where UserRoleChildId = ur.UserRoleId and IsDeleted = 0) as NumberOfParents FROM Role r, UserRole ur LEFT JOIN User u ON u.UserId = ur.UserId WHERE ur.RoleId = r.RoleId and ur.UserRoleId in (select UserRoleChildId from UserRoleHierarchy where UserRoleParentId = $parentId and IsDeleted = 0) and ur.IsDeleted = 0";
-    $result = execute($sql);
-    while ($row = $result->fetch_assoc()) {
         $o['UserRoleId'] = $row['UserRoleId'];
         $o['FirstName'] = $row['FirstName'];
         $o['LastName'] = $row['LastName'];
@@ -109,17 +149,61 @@ function getUserRoleChildren($children, $parentId) {
         $o['Address'] = $row['Address'];
         $o['Phone'] = $row['Phone'];
         $o['Role'] = $row['Description'];
-        $o['Title'] = $row['Title'];
         $o['Image'] = $row['Image'];
         $o['LastActiveDate'] = $row['LastActiveDate'];
+        $o['Title'] = $row['Title'];
         $o['UserId'] = $row['UserId'];
         $o['IsAssigned'] = !empty($row['UserId']);
-        $o['NumberOfParents'] = $row['NumberOfParents'];
-        $o['UserRoleParent'] = $parentId;
         $o['RoleId'] = $row['RoleId'];
         $o['Children'] = array();
         $o['Children'] = getUserRoleChildren($o['Children'], $o['UserRoleId']);
-        array_push($children, $o);
+
+        $conn->close();
+        return encode(true, $o);
+    } else {
+        return encode(false, var_dump($conn->error));
     }
-    return $children;
+
+}
+
+function getUserRoleChildren($children, $parentId) {
+    $conn = connect();
+    $q="SELECT *, (select count(*) from UserRoleHierarchy where UserRoleChildId = ur.UserRoleId and IsDeleted = 0) as NumberOfParents FROM Role r, UserRole ur LEFT JOIN User u ON u.UserId = ur.UserId WHERE ur.RoleId = r.RoleId and ur.UserRoleId in (select UserRoleChildId from UserRoleHierarchy where UserRoleParentId = ? and IsDeleted = 0) and ur.IsDeleted = 0";
+    if ($stmt = $conn->prepare($q)) {
+        $children = array();
+        $stmt->bind_param("i", $parentId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        $conn->close();
+        if ($result->num_rows > 0) {
+            foreach ($result->fetch_assoc() as $row)
+            {
+                $o['UserRoleId'] = $row['UserRoleId'];
+                $o['FirstName'] = $row['FirstName'];
+                $o['LastName'] = $row['LastName'];
+                $o['Name'] = $row['FirstName'] . ' ' . $row['LastName'];
+                $o['Username'] = $row['Username'];
+                $o['Address'] = $row['Address'];
+                $o['Phone'] = $row['Phone'];
+                $o['Role'] = $row['Description'];
+                $o['Title'] = $row['Title'];
+                $o['Image'] = $row['Image'];
+                $o['LastActiveDate'] = $row['LastActiveDate'];
+                $o['UserId'] = $row['UserId'];
+                $o['IsAssigned'] = !empty($row['UserId']);
+                $o['NumberOfParents'] = $row['NumberOfParents'];
+                $o['UserRoleParent'] = $parentId;
+                $o['RoleId'] = $row['RoleId'];
+                $o['Children'] = array();
+                $o['Children'] = getUserRoleChildren($o['Children'], $o['UserRoleId']);
+                array_push($children, $row);
+            }
+            return $children;
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
 }
